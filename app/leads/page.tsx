fa8a46e96fffaf13573cfb6d7d465ece03f35c8e
jaforsheikh <jaforsheikh258@gmail.com>
2026-04-29 13:41:38 +0600

    fixed

---
+++ app/leads/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LeadsPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-2xl font-bold">Leads Page</h1>
+    </div>
+  );
+}
